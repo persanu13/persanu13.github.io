777174b0a384122c55331d4d91670306ef79fd01
persanu13 <86602112+persanu13@users.noreply.github.com>
2021-06-28 14:50:36 +0300

    Delete Blog site tip directory

--- Blog site tip/newfile.php
+++
@@ -1,3 +0,0 @@
-   echo 'ceva';       echo 'ceva 20' ;       echo 'ceva 20';  
-           20
-         
--- Blog site tip/paginiarticole/cod1/refresh.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-
-header('Location:../'.$_SESSION['numeArticol'].'.php');
-
-?>
